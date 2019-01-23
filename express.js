28476a83469d88638fa1833abd8a8631b739ab85
Vee Upatising <veeupatising@gmail.com>
2019-01-22 23:22:06 -0500

    Add files via upload
    update HTML and CSS of some pages

--- express.js
+++ express.js
@@ -1,23 +1,27 @@
 var express = require("express");
 var app = express();
+//telling app to use files in public directory
+app.use(express.static("public"));
 
+//loads home page from ejs html file
 app.get("/", function(req, res){
-    res.send("HOMEPAGE");
+    res.render("home.ejs");
 })
 
 app.get("/bye", function(req, res){
-    res.send("Bye Page");
+    res.render("bye.ejs");
 })
 
 app.get("/woof", function(req, res){
     res.send("woof page");
 })
 
+//will load with anything following /woof/
 app.get("/woof/:awoo", function(req, res){
     var name = req.params.awoo;
     res.send("Welcome to to the " + name + " page");
 })
-
+//will load if none of the other pages load
 app.get("*", function(req, res){
     res.send("The code monkeys have not coded this page in yet");
 })
